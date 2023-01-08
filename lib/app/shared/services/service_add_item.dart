9487f5f3118fd15e8928ad1e1e5a704602acdb01
Vitinho-Dev <victorhenrique121413@gmail.com>
2023-01-08 16:07:46 -0300

    service removido

--- lib/app/shared/services/service_add_item.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: depend_on_referenced_packages
-
-import 'package:dartz/dartz.dart';
-
-import '../../modules/item_module/domain/entities/item_entity.dart';
-
-abstract class ServiceAddItem {
-  Future<Unit> addItem(Item item);
-}
